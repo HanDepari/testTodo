f5f8ce4c225e63a333795347696b69db63844c01
begiliapa <alfentinojr2905@gmail.com>
2024-12-13 16:56:25 +0700

    the biggining

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+Route::get('/', function () {
+    $name = "John Doe";
+    return view('dashboard', compact('name'));
 });

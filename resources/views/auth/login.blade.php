4ed4626a49ab6d8d00b1c7543286600650e41fac
HanDepari <hagaibastanta655@gmail>
2024-12-15 20:27:39 +0700

    berubah

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 <body>
     <x-bladewind::card>
-        <h1>Bladewind Card Component</h1>
+        <h1>Todobro</h1>
         <form method="get" class="signup-form">
             <h1 class="my-2 text-2xl font-light text-blue-900/80">Login</h1>
             <p class="mt-3 mb-6 text-blue-900/80 text-sm">
